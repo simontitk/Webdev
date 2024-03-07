003963f998dc8e4dcf0948d7002c83a626b47261
TobiasLysdal <tolh@itu.dk>
2024-03-07 10:54:31 +0100

    latest additions userinfo and cross page functionality

--- src/scripts/userinfo.js
+++ src/scripts/userinfo.js
@@ -1,5 +1,5 @@
 function displayAccountInfo(){
-
+        console.log('LocalStorage Data:', localStorage);
         //display the information in the various spans replace document, with where the info is stored
         document.getElementById('usernameHeading-profile').innerText = localStorage.getItem("usernameProfile") || '';
         document.getElementById('username').innerText = localStorage.getItem("usernameProfile");
@@ -8,16 +8,16 @@ function displayAccountInfo(){
         document.getElementById('sNameProfile').innerText = localStorage.getItem("lname");
         document.getElementById('phoneNumberProfile').innerText = localStorage.getItem("phonenumber");
         document.getElementById('passwordProfile').innerText = localStorage.getItem("password");
+        document.getElementById('addressHome').innerText = localStorage.getItem("addressHome");
         
-        document.getElementById('homeAddressInfo').innerText = accountInfoData.homeaddress;
-        document.getElementById('homeZipCode').innerText = accountInfoData.homezipcode;
-        document.getElementById('homeAddressCountry').innerText = accountInfoData.homecountry;
-        document.getElementById('officeAddressInfo').innerText = accountInfoData.officeaddress;
-        document.getElementById('officeZipCode').innerText = accountInfoData.officezipcode;
-        document.getElementById('officeAddressCountry').innerText = accountInfoData.officecountry;
+        // document.getElementById('homeZipCode').innerText = accountInfoData.homezipcode;
+        // document.getElementById('homeAddressCountry').innerText = accountInfoData.homecountry;
+        // document.getElementById('officeAddressInfo').innerText = accountInfoData.officeaddress;
+        // document.getElementById('officeZipCode').innerText = accountInfoData.officezipcode;
+        // document.getElementById('officeAddressCountry').innerText = accountInfoData.officecountry;
     }
 
-    function addUserName() { /* username showcased in profile - adjustments needed to work globally */
+    function addUserName() { /* username showcased in profile - adjustments needed to work globally more functions?*/
         const userName = localStorage.usernameProfile;
         if (userName !== undefined && userName !== null) {
         document.getElementById("usernameHeading-profile").innerText =
@@ -29,32 +29,25 @@ function displayAccountInfo(){
 
 
     function store() {
-        // get first name
-        let inputFname = document.getElementById("fname");
-        // save first name
-        localStorage.setItem("fname", inputFname.value);
-        // get last name
-        let inputLname = document.getElementById("lname");
-        // save last name
-        localStorage.setItem("lname", inputLname.value);
-        //
+        // get values from form
+        let inputFname = document.getElementById("fname").value;
+        let inputLname = document.getElementById("lname").value;
         let inputUsername = document.getElementById("usernameProfile");
-        //
-        localStorage.setItem("usernameProfile", inputUsername.value);
-        //
-        let inputEmail = document.getElementById("email");
-        //
-        localStorage.setItem("email", inputEmail.value);
-        //
+        let inputEmail = document.getElementById("email").value;
         let inputNumber = document.getElementById("phonenumber");
-        //
-        localStorage.setItem("phonenumber", inputNumber.value);
-        //
         let inputPassword = document.getElementById("password");
-        //
+        let inputAddressHome = document.getElementById("addressHome").value;
+
+        //Saves values to local storage
+        localStorage.setItem("fname", inputFname);
+        localStorage.setItem("lname", inputLname);
+        localStorage.setItem("usernameProfile", inputUsername.value);
+        localStorage.setItem("email", inputEmail);
+        localStorage.setItem("phonenumber", inputNumber.value);
         localStorage.setItem("password", inputPassword.value);
+        localStorage.setItem("addressHome", inputAddressHome);
 }
-    document.body.onload = addUserName(); // attaching the event handler- addUserName function​​
+ // attaching the event handler- addUserName function​​
     //resetLocal stored data.
     function resetLocalStorage() {
         localStorage.clear();
